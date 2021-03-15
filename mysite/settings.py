f0a533680740c0ae5440dd671f579e3a88cdea30
unknown <bELMUT695>
2021-03-15 15:06:51 -0500

    mi primer comit en heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,6 +147,3 @@ JWT_AUTH = {
 }
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-django_heroku.settings(locals())
